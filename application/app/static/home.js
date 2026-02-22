56c0764d48516634810b1b8a41a5c544047b3500
kendallparry <156971407+kendallparry@users.noreply.github.com>
2026-02-22 12:52:00 -0800

    fixed sidebar

--- application/app/static/home.js
+++ application/app/static/home.js
@@ -7,6 +7,25 @@ function redirect(pageName){
 }
 async function loadUpcomingEvents() {
     const data = await loadEvents();
+
+    //load sidebar events list
+    const upcomingUl = document.querySelector('#upcoming ul');
+    upcomingUl.innerHTML = '';
+    data.forEach(date => {
+        const dateLi = document.createElement('li');
+        dateLi.textContent = date.date;
+        upcomingUl.appendChild(dateLi);
+
+        const itemsUl = document.createElement('ul');
+        date.items.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.startTime} - ${item.endTime} -- ${item.title}`;
+            itemsUl.appendChild(li);
+        });
+        upcomingUl.appendChild(itemsUl);
+    });
+
+    //load main events list
     const ul = document.querySelector('#eventsList ul');
     ul.innerHTML = '';
 
