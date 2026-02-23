3abf8d86e7f97081dd3f72e5f8e3080610c13622
kendallparry <156971407+kendallparry@users.noreply.github.com>
2026-02-23 12:56:46 -0800

    working on redirects

---
+++ application/app/static/redirects.js
@@ -0,0 +1,9 @@
+const logoButton = document.getElementById("logoButton");
+logoButton.addEventListener('click', function() {
+    window.location.href = '/home';
+})
+
+const settingsButton = document.getElementById("settingsButton");
+settingsButtonButton.addEventListener('click', function() {
+    window.location.href = '/settings';
+})
