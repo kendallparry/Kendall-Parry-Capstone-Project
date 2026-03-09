11b922dbdd4521f981ef5e70d63cb930ededd463
kendallparry <156971407+kendallparry@users.noreply.github.com>
2026-03-08 18:34:33 -0700

    adding react hopefully

--- application/app/static/redirects.js
+++ application/app/static/redirects.js
@@ -1,7 +1,7 @@
-const logoButton = document.getElementById("logoButton");
-logoButton.addEventListener('click', function() {
-    window.location.href = '/';
-})
+// const logoButton = document.getElementById("logoButton");
+// logoButton.addEventListener('click', function() {
+//     window.location.href = '/';
+// })
 
 const settingsButton = document.getElementById("settingsButton");
 settingsButton.addEventListener('click', function() {
