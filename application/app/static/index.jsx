aa10022107bfaf47108c26efe01e71c93800ca28
kendallparry <156971407+kendallparry@users.noreply.github.com>
2026-03-08 18:33:40 -0700

    adding react hopefully

--- application/app/static/index.jsx
+++ application/app/static/index.jsx
@@ -4,12 +4,13 @@ const navbar = createRoot(document.getElementById("navbar"))
 
 function NavBar(){
     return ( 
-        <nav className="navbar">
-            <div className="container-fluid align-items-center">
-                <span className="navbar-brand" href="#"><img src="../static/BareBonesLogo_White.svg" width="150px"/></span>
-                <span className="nav-item" href="#"> <img src="../static/icons/settings.svg"/></span>
-            </div>
-        </nav>
+        // <nav className="navbar">
+        //     <div className="container-fluid align-items-center">
+        //         <span className="navbar-brand" href="#"><img src="../static/BareBonesLogo_White.svg" width="150px"/></span>
+        //         <span className="nav-item" href="#"> <img src="../static/icons/settings.svg"/></span>
+        //     </div>
+        // </nav>
+        <nav style={{background: "red"}}>TEST NAVBAR</nav>
     )
 }
 
